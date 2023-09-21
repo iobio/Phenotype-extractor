d3a8bd9cd8f95da94c2d6aa97914ab992a604aab
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2023-09-21 16:54:24 -0600

    remove HPO_phenotypes
    This seems to not be referenced in this package.

--- cli.js
+++ cli.js
@@ -8,8 +8,11 @@ var fileName = args[0];
 
 fs.readFile(fileName, 'utf-8', ((err, inputData) => {
   var processedData = processData(inputData);
+
   processedData.then(data => {
+
     var resp = util.inspect(data, { maxArrayLength: null })
+    
     console.log(JSON.stringify(resp));
     return JSON.stringify(resp);
   })
